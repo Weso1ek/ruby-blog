48430350231d81d8ab3247d3c955dd49224a93d3
Sebastian Wesołowski <Sebastian.Wesolowski@grupawp.pl>
2021-09-05 12:02:16 +0200

    Articles + comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  root "articles#index"
+
+  #get "/articles", to: "articles#index"
+  #get "/articles/:id", to: "articles#show"
+
+  resources :articles do
+    resources :comments
+  end
+
+  #resources :articles
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
